b505202571b1519a1ba114dac03f584a6a74c9fd
alcancexbrasil-venan <alcancexbrasil@gmail.com>
2026-05-15 14:46:02 -0300

    Update index.js

--- api/index.js
+++ api/index.js
@@ -1,7 +1,7 @@
 export default async function handler(req, res) {
   try {
     const response = await fetch(
-      "https://rhnijvtwnpmdyftfyoyiq.supabase.co/functions/v1/site-render"
+      "https://rhniytwnpmdytftyoyiq.supabase.co/functions/v1/site-render"
     );
 
     const html = await response.text();
@@ -9,7 +9,9 @@ export default async function handler(req, res) {
     res.setHeader("Content-Type", "text/html; charset=utf-8");
 
     return res.status(200).send(html);
+
   } catch (error) {
+
     console.error(error);
 
     return res.status(500).send("Proxy error");
